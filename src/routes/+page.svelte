467aef8c9a6052b889a6b0724f85d1e76ddf3291
nomideusz <b.dymet@gmail.com>
2025-11-19 00:04:16 +0100

    Update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -700,7 +700,7 @@
 				<span style="color: #FF6B9D;">{translate('home.video.headingAccent')}</span>
 				<span class="flex items-center gap-3 md:gap-4">
 					<span style="color: #ffffff;">{translate('home.video.headingEnd').split(' ')[0]}</span>
-					<img src="/logo.png" alt="Pikastro" class="h-12 md:h-20 lg:h-24 w-auto inline-block" style="padding: 15px 0 0 5px;" />
+					<img src="/logo.png" alt="Pikastro" class="h-12 md:h-20 lg:h-24 w-auto inline-block" style="padding: 17px 0 0 5px;" />
 				</span>
 			</h2>
 			<p class="text-xl md:text-3xl text-gray-200" style="text-shadow: 0 0 15px rgba(39, 39, 91, 0.8), 0 0 30px rgba(39, 39, 91, 0.6);">
